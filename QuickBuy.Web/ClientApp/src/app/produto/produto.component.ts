36b0b30b04dbf9ee097765c57b7dde8c42191f8b
Regis Nogueira <regis.nogueira@avalara.com>
2020-06-14 20:41:18 -0300

    menu produto

---
+++ QuickBuy.Web/ClientApp/src/app/produto/produto.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+
+//decorator @Component que vai configurar o ProdutoComponent
+//nome da tag html que o ProdutoComponent vai ser renderizado
+@Component({
+  selector: "app-produto",
+  template: "<html><body>{{ obterNome() }}</body></html>"
+})
+
+//depois adicionar import no app.module.ts
+export class ProdutoComponent { 
+  public nome: string;
+  public liberadoParaVenda: boolean;
+
+  public obterNome(): string{
+    return "Samsung teste";
+  }
+   
+}
