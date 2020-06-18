c305c8825d5b03464a6a84487539aab30794f8e1
Regis Nogueira <regis.nogueira@avalara.com>
2020-06-17 22:24:06 -0300

    undefinedapi

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Usuario } from "../../modelo/usuario";
 import { Router, ActivatedRoute } from '@angular/router';
+import { UsuarioServico } from "../../servicos/usuario/usuario.servico";
 
 @Component({
   selector: "app-login",
@@ -13,9 +14,8 @@ export class LoginComponent implements OnInit {
   public returnUrl: string;
 
   //passando como parametro no construtor, esta injetando a dependencia
-  constructor(private router: Router, private activatedRouter: ActivatedRoute) {
-    
-    
+  constructor(private router: Router, private activatedRouter: ActivatedRoute, private usuarioServico: UsuarioServico) {
+
   }
 
   ngOnInit(): void {
@@ -25,10 +25,19 @@ export class LoginComponent implements OnInit {
 
 
   entrar() {
-    if (this.usuario.email == "email@email.com" && this.usuario.senha == "senha") {
-      sessionStorage.setItem("usuario-autenticado", "1");
-      this.router.navigate([this.returnUrl]);
-    }
+    //METODO QUE TEM RETORNO O OBSERVABLE
+    //no "subscribe" que de fato vai iniciar a chama ao serviço web no ASPNet.Core
+    this.usuarioServico.verificarUsuario(this.usuario)
+      .subscribe(
+        data => {
+        },
+        err => {
+        });
+
+    //if (this.usuario.email == "email@email.com" && this.usuario.senha == "senha") {
+    //  sessionStorage.setItem("usuario-autenticado", "1");
+    //  this.router.navigate([this.returnUrl]);
+    //}
   }
 
 }
---
+++ QuickBuy.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickBuy.Dominio.Entidades;
+using System;
+
+namespace QuickBuy.Web.Controllers
+{
+    [Route("api/[Controller]")]
+    public class UsuarioController: Controller
+    {
+        [HttpPost]
+        public ActionResult Post()
+        {
+            try
+            {
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        [HttpPost("VerificarUsuario")]
+        public ActionResult VerificarUsuario([FromBody] Usuario usuario)
+        {
+            try
+            {
+                if(usuario.Email == "email@email.com" && usuario.Senha == "senha")
+                    return Ok(usuario);
+                
+                return BadRequest("Usuário ou senha inválido");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+    }
+}
